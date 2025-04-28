7cf93621028f1081624a238209700d081a56bb9c
oshadi <bawanioshadi0@gmail.com>
2025-04-28 20:21:43 +0530

    created question 03

---
+++ Q3/Calculator.java
@@ -0,0 +1,17 @@
+class Calculator {
+    // Add two integers
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Overloaded method: add three integers
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Overloaded method: add two doubles
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
